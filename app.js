f3993c983c36eb475cc489ac6a1c32b18f1d1fb3
GersonDetroy <gerdanman2.5@gmail.com>
2025-04-10 20:41:01 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Función para mostrar mensajes
+const showMessage = (message) => message;
+
+// Mostrar mensaje
+console.log(showMessage("Hola cabeza de bola"));
+console.log("Hola Gerson");
+
+// Variables y suma
+var a = 7;
+var b = 10;
+
+var r = a + b;
+
+// Mostrar resultado con interpolación de cadenas
+console.log(`el resultado es:${r}`);
+console.log("Ya lo enlace");
