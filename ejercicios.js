fb1eb9d22d55accff60eb15d18e0bce2b2fa5605
GersonDetroy <gerdanman2.5@gmail.com>
2025-04-10 21:57:57 -0500

    tercer commit

---
+++ ejercicios.js
@@ -0,0 +1,38 @@
+function sumar(a, b) {
+    // Tu código aquí
+  }
+  
+  console.log(sumar(3, 4)); // Resultado esperado: 7
+  console.log(sumar(10, 20)); // Resultado esperado: 30
+
+  
+  function esPar(num) {
+    // Tu código aquí
+  }
+  
+  console.log(esPar(4)); // true
+  console.log(esPar(7)); // false
+
+  
+  function celsiusAFahrenheit(c) {
+    // Tu código aquí
+  }
+  
+  console.log(celsiusAFahrenheit(0));   // 32
+  console.log(celsiusAFahrenheit(25));  // 77
+
+  
+  function mayor(a, b) {
+    // Tu código aquí
+  }
+  
+  console.log(mayor(5, 8));   // 8
+  console.log(mayor(20, 10)); // 20
+
+  
+  function repetirTexto(texto, n) {
+    // Tu código aquí
+  }
+  
+  console.log(repetirTexto("Hola ", 3)); // "Hola Hola Hola "
+  
